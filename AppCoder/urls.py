acbdab48939627248ac1b2a305f844eaca966c31
Pablo Grigolato <pablogrigolato@gmail.com>
2023-03-08 20:39:06 -0300

    Segunda versión

---
+++ AppCoder/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from AppCoder import views
+
+urlpatterns = [
+    path('', views.inicio),
+    path('cursos', views.cursos),
+    path('profesores', views.profesores),
+    path('estudiantes', views.estudiantes),
+    path('entregables', views.entregables),
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from AppCoder.models import Curso
 from django.http import HttpResponse
 
 # Create your views here.
-def curso(self):
-    curso = Curso(nombre='Desarrollo Web', camada=12345)
-    curso.save()
-    documentoDeTexto = f'--->Curso: {curso.nombre} Camada: {curso.camada}'
-    return HttpResponse(documentoDeTexto)
+#def curso(self):
+#    curso = Curso(nombre='Desarrollo Web', camada=12345)
+#    curso.save()
+#    documentoDeTexto = f'--->Curso: {curso.nombre} Camada: {curso.camada}'
+#    return  HttpResponse(documentoDeTexto)
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def cursos(request):
+    return render(request, 'cursos.html')
+
+def profesores(request):
+    return render(request, 'profesores.html')
+
+def estudiantes(request):
+    return render(request, 'estudiantes.html')
+
+def entregables(request):
+    return render(request, 'entregables.html')
